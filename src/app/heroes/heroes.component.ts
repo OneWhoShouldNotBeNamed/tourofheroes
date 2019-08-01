9a593b6660a67fcab00ece50d24e8ac1523a3d31
OneWhoShouldNotBeNamed <maheshsk019@gmail.com>
2019-08-01 17:12:50 +0530

    Finished observables

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
 import {HEROES} from '../mock-heros'
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes=HEROES;
+  //heroes=HEROES;
+  heroes: Hero[];
   selectedHero:Hero;
-  constructor() { }
+  //constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  //  this.heroes = this.heroService.getHeroes();
+    
+
   }
 
-  onSelect (hero:Hero):void{
+  /*onSelect (hero:Hero):void{
     this.selectedHero=hero;
+  }*/
+ /* getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }*/
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
